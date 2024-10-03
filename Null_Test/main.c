86678816f9c75354358dbfe7e710c8e8281407f5
ecaterina99 <tricolicikatea1999@gmail.com>
2024-10-03 16:08:49 +0300

    Update main.c

--- Null_Test/main.c
+++ Null_Test/main.c
@@ -3,11 +3,12 @@
 int main(void) {
     int a = 5;
     int *ptr = NULL;
-
+    printf("a=%d\n", a);
     ptr = &a;
-
+    printf("*ptr=%d\n", *ptr);
     if (ptr != NULL) {
         *ptr = 1;
     }
+    printf("a=%d\n", a);
     return 0;
 }
